19b66d8a094768bc754a740590c87138687e4034
Abhiras5 <rskshete@gmail.com>
2026-01-13 22:34:09 +0530

    DSA - minMax element of an array

---
+++ grind-DSA/minMaxArray.js
@@ -0,0 +1,20 @@
+const arr = [1, 0, 1, 3, 13, 12, 26, 33, 49];
+
+function findMinMax(arr) {
+  let minElement = Infinity; // Starts with the highest possible number (Infinity) as the initial minimum
+  let maxElement = -Infinity; // Starts with the lowest possible number (-Infinity) as the initial maximum
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < minElement) {
+      minElement = arr[i];
+    }
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+
+  return { min: minElement, max: maxElement };
+}
+
+console.log(findMinMax(arr));
+// Time Complexity: O(n) - Linear, as it scans the array once.
